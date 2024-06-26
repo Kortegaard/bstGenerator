53c16d3fd5574f555aa0a3c745a0de2de2a06464
Anders Kortegaard <ank@askapps.eu>
2024-06-27 00:02:30 +0200

    modifying similar labels + longest label

--- functionBuilder/functionBuilder.go
+++ functionBuilder/functionBuilder.go
@@ -106,7 +106,7 @@ func WriteLabelConstructor() string{
 func BibBegin() string{
     return `FUNCTION {bib.begin}
 {
-    "\begin{thebibliography}{ABC99}" write$ newline$
+    "\begin{thebibliography}{" longest.label "}" * * write$ newline$
     "\newcommand*{\doi}[1]{\href{https://doi.org/#1}{\sloppy #1}}" write$ 
     newline$
     newline$
@@ -117,4 +117,104 @@ func BibEnd() string{
 {
     "\end{thebibliography}" write$ newline$
 } `
+}
+
+
+func Sorter() string{
+    return `FUNCTION {format.authors.sort}{
+    author 's :=
+    s num.names$ 'numnames :=
+    ""
+    #1 'nameptr :=
+    { nameptr numnames #1 + < }
+    {
+        s nameptr "{ll}{ff}" format.name$
+        *
+        nameptr numnames #1 - =    % if last number
+        {" and " *}
+        {", " *}
+        if$
+        nameptr #1 + 'nameptr :=
+    }
+    while$
+} 
+
+FUNCTION {presort}
+{
+    format.authors.sort 
+    "     "  *
+    year *
+    "     "  *
+    title *
+    'sort.key$ :=
+}
+
+ITERATE {presort}`
+}
+
+
+func LabelDifferentiator() string{
+    return `
+    STRINGS {last.label} 
+INTEGERS {addon.chr.track last.label.had.addon}
+
+FUNCTION {init.label.passes}{
+    "" 'last.label :=
+    #0 'addon.chr.track :=
+    #0 'last.label.had.addon :=
+}
+
+FUNCTION {label.forward} {
+    label last.label =
+    {
+        label 'last.label :=
+        label addon.chr.track "b" chr.to.int$ + int.to.chr$  * 'label :=
+        addon.chr.track #1 + 'addon.chr.track  :=
+        #1 'label.addon :=
+
+    }{
+        label 'last.label :=
+        #0 'addon.chr.track :=
+        #0 'label.addon :=
+    }
+    if$
+}
+
+FUNCTION {label.backwards} {
+    #1 last.label.had.addon 
+    label.addon #1 < and =
+    {
+        label "a" * 'label :=
+        #1 'label.addon :=
+    }{ }
+    if$
+    label.addon 'last.label.had.addon :=
+}
+
+EXECUTE {init.label.passes}
+ITERATE {label.forward}
+REVERSE {label.backwards}`
+}
+
+func FindLongestLabel() string{
+    return `INTEGERS {longest.label.width}
+STRINGS {longest.label}
+
+FUNCTION {initiate.longest.label}{
+    #0 'longest.label.width :=
+    "" 'longest.label :=
+}
+
+FUNCTION {longest.label.search} {
+    label text.length$ longest.label.width >
+    {
+        label 'longest.label :=
+        label text.length$ 'longest.label.width :=
+    }{}
+    if$
+}
+
+EXECUTE {initiate.longest.label}
+ITERATE {longest.label.search}`
+
 }
